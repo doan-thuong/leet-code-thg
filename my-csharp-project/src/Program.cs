a99686d001d04ec298a108cbebf4a1f0a2d9b633
ThuongDT <thuongdt@ezg.vn>
2025-12-01 11:37:21 +0700

    Search Insert Position (Otherwise, return its insertion index)

--- my-csharp-project/src/Program.cs
+++ my-csharp-project/src/Program.cs
@@ -816,6 +816,27 @@ public class Solution
 
         return new int[] { indexMin, indexMax };
     }
+    public int SearchInsert(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length - 1;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        return l;
+    }
 
     public static void Main(string[] args)
     {
